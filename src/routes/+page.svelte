6480a3f573b99ce74b5db4a91ec527daa8ebb312
Grant Pitt <grantpitt0@gmail.com>
2024-05-01 16:30:56 -0400

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,133 @@
+<script lang="ts">
+  // let file: File | null = null;
+  // let image: HTMLImageElement | null = null;
+  type fileInfo = {
+    image: HTMLImageElement;
+    width: number;
+    height: number;
+  };
+
+  let fileInfo: fileInfo | null = null;
+
+  type FileUploadEvent = Event & {
+    target: HTMLInputElement & EventTarget;
+  };
+
+  async function handleFileUpload(event: FileUploadEvent) {
+    const file = event?.target?.files?.[0];
+    if (!file) {
+      return;
+    }
+    console.log(file);
+
+    const image = await createImage(await readFileAsDataURL(file));
+    // console.log(`Width: ${image.width}, Height: ${image.height}`);
+    fileInfo = {
+      image: image,
+      width: image.width,
+      height: image.height,
+    };
+    console.log(image);
+  }
+
+  function readFileAsDataURL(file: File): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = () => resolve(reader.result as string);
+      reader.onerror = () => reject(reader.error);
+      reader.readAsDataURL(file);
+    });
+  }
+
+  function createImage(dataURL: string): Promise<HTMLImageElement> {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.onload = () => resolve(img);
+      img.onerror = () => reject(new Error("Error loading image"));
+      img.src = dataURL;
+    });
+  }
+
+  function truncate(value: number, decimals = 1) {
+    return Math.trunc(value * 10 ** decimals) / 10 ** decimals;
+  }
+</script>
+
+<main class="mx-auto my-8 px-4 max-w-3xl">
+  <h1 class="font-medium text-3xl my-6">Fize Size Tool</h1>
+
+  <section class="my-6">
+    <label for="artwork">Upload your design file:</label>
+    <input
+      type="file"
+      id="artwork"
+      name="artwork"
+      accept="image/png, image/jpeg"
+      on:change={handleFileUpload}
+    />
+  </section>
+
+  {#if fileInfo}
+    <img src={fileInfo.image.src} alt="Uploaded file" class="max-w-96 max-h-96 my-6" />
+
+    <h2 class="font-medium text-xl my-2">File Dimensions</h2>
+    <p>Width: {fileInfo.width} px</p>
+    <p>Height: {fileInfo.height} px</p>
+
+    <h2 class="font-medium text-xl mt-6 mb-2">Recommended Print Sizes</h2>
+    <table class="mb-6">
+      <thead>
+        <tr>
+          <th class="px-3 py-2 text-left text-sm font-medium tracking-wider border border-gray-300"></th>
+          <th class="px-3 py-2 text-left text-sm font-medium tracking-wider border border-gray-300"
+            >Standard Website Quality (72 DPI)</th
+          >
+          <th class="px-3 py-2 text-left text-sm font-medium tracking-wider border border-gray-300"
+            >Low Quality Print (100 DPI)</th
+          >
+          <th class="px-3 py-2 text-left text-sm font-medium tracking-wider border border-gray-300"
+            >Medium Quality Print (200 DPI)</th
+          >
+          <th class="px-3 py-2 text-left text-sm font-medium tracking-wider border border-gray-300"
+            >High Quality Print (300 DPI)
+            <span class="bg-pink-200 rounded-md px-1">*Recommended</span>
+          </th>
+        </tr>
+      </thead>
+      <tbody class="bg-white divide-y divide-gray-200">
+        <tr>
+          <td class="px-3 py-2 whitespace-nowrap text-sm border border-gray-300 font-medium">Width (inches)</td>
+          <td class="px-3 py-2 whitespace-nowrap text-sm border border-gray-300"
+            >{truncate(fileInfo.width / 72)}"</td
+          >
+          <td class="px-3 py-2 whitespace-nowrap text-sm border border-gray-300"
+            >{truncate(fileInfo.width / 100)}"</td
+          >
+          <td class="px-3 py-2 whitespace-nowrap text-sm border border-gray-300"
+            >{truncate(fileInfo.width / 200)}"</td
+          >
+          <td class="px-3 py-2 whitespace-nowrap text-sm border border-gray-300"
+            >{truncate(fileInfo.width / 300)}"</td
+          >
+        </tr>
+        <tr>
+          <td class="px-3 py-2 whitespace-nowrap text-sm border border-gray-300 font-medium">Height (inches)</td>
+          <td class="px-3 py-2 whitespace-nowrap text-sm border border-gray-300"
+            >{truncate(fileInfo.height / 72)}"</td
+          >
+          <td class="px-3 py-2 whitespace-nowrap text-sm border border-gray-300"
+            >{truncate(fileInfo.height / 100)}"</td
+          >
+          <td class="px-3 py-2 whitespace-nowrap text-sm border border-gray-300"
+            >{truncate(fileInfo.height / 200)}"</td
+          >
+          <td class="px-3 py-2 whitespace-nowrap text-sm border border-gray-300"
+            >{truncate(fileInfo.height / 300)}"</td
+          >
+        </tr>
+      </tbody>
+    </table>
+
+    <p>In order for printed materials to look crisp and clear, it is recommended to use high resolution images. The resolution for printed materials is measured in DPI (dots per inch). The higher the DPI, the better the quality of the printed image.</p>
+  {/if}
+</main>
